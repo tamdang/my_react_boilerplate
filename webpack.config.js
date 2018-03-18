579e28c56f75f7d2e842bd9c9058176f8a6476e6
Dang Thanh Tam <dangtam@gmail.com>
2018-03-18 20:53:51 +0700

    Add babel-preset-react, babel-loader
    Update component using JSX
    Add babel config file to compile JSX
    Update webpack config to use babel-loader to load js files
    Add mode=development into webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,11 @@ module.exports = {
   entry: './app/index.js',
   output: {
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+    ]
+  },
+  mode: 'development',
 }
