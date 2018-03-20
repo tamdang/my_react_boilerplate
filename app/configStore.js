4e4f9b83898af5be9dd562b0f970053174877174
Dang Thanh Tam <dangtam@gmail.com>
2018-03-21 05:29:28 +0700

    step 8
    step 8

--- app/configStore.js
+++ app/configStore.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store
